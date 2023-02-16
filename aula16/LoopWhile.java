505d2a1b7ed230109c6d47133c574fea092ed0fe
KevenMartins <124926761+KevenMartins@users.noreply.github.com>
2023-02-16 11:51:52 +0000

    loop while

---
+++ aula16/LoopWhile.java
@@ -0,0 +1,29 @@
+package com.keven.cursojava.aula16;
+
+public class LoopWhile {
+
+	public static void main(String[] args) {
+		
+		int i = 1; // count ou cont
+		int max = 10;
+		
+		System.out.print("Contando até " + max);
+		
+		while(i <= max){
+			
+			System.out.println("Valor de i: " + i);
+			i++; // i = i + 1; ou i += 1;
+		}
+		
+		System.out.println(i); //valor de 11
+		
+		do {
+			i++;	
+			System.out.println("Valor de i: " + i);
+		}while(i <15);
+		
+		System.out.println(i); //valor de 15
+		
+	}
+
+}

dfa706e7929af443b2c4c88ab566f5ad4e9f59c6
KevenMartins <124926761+KevenMartins@users.noreply.github.com>
2023-02-16 11:48:39 +0000

    variáveis

---
+++ aula10/Variaveis.java
@@ -0,0 +1,34 @@
+package com.keven.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+
+		//convenção java
+		int idade;
+		String nome; //não é um tipo primitivo, é uma classe
+		String nomeDoMeuCao;
+		
+		
+		//aceito mas não utilizado
+		int _idade;
+		int $idade;
+		
+		//Não é convenção java
+		String ano2014;
+		String nome_do_meu_Cao;
+		String NomeDoMeuCao;
+		
+		
+		//má pratica - nome de variaveis 
+		int a = 10;
+		String b = "keven";
+		
+		nome ="keven";
+		
+		
+		System.out.print("Nome: " + nome);
+
+	}
+
+}

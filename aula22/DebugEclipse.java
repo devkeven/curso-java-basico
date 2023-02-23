ce5cd4d1b61d6591797e7a3ae5cb358597350b7d
KevenMartins <124926761+KevenMartins@users.noreply.github.com>
2023-02-23 13:44:46 +0000

    Debug

---
+++ aula22/DebugEclipse.java
@@ -0,0 +1,15 @@
+package com.keven.cursojava.aula22;
+
+public class DebugEclipse {
+
+	public static void main(String[] args) {
+		
+		int[] notas = new int[3];
+		
+		for(int i=0; i<notas.length; i++) {
+			notas[i] = i;
+		}
+
+	}
+
+}

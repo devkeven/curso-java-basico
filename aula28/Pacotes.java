4743a21ac4a96c56761325202206287167348126
KevenMartins <124926761+KevenMartins@users.noreply.github.com>
2023-02-27 10:13:30 +0000

    Pacotes e import

---
+++ aula28/Pacotes.java
@@ -0,0 +1,21 @@
+package com.keven.cursojava.aula28;
+
+import com.keven.cursojava.aula20.labs.Exer05;
+import com.keven.cursojava.aula24.labs.Exer06;
+import com.keven.cursojava.aula27.Carro;
+
+public class Pacotes {
+	
+	public static void main(String[] args) {
+		
+		
+		Carro carro;
+		Carro carro2;
+		
+		Exer06 exer06; 
+		Exer05 exer05;
+		
+		
+	}
+
+}

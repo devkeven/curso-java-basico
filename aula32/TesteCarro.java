1ba427386f539bcd36378e82bcc28c25197757cc
KevenMartins <124926761+KevenMartins@users.noreply.github.com>
2023-02-27 11:18:07 +0000

    Encapsulamento(getters e setters)

---
+++ aula32/TesteCarro.java
@@ -0,0 +1,18 @@
+package com.keven.cursojava.aula32;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro van = new Carro();
+		
+		//van.marca = "Fiat";
+		
+		van.setMarca("Fiat");
+		
+		System.out.println(van.getMarca());
+		
+
+	}
+
+}

1407577a9ae43b914ba3bd99a249a63221003b01
KevenMartins <124926761+KevenMartins@users.noreply.github.com>
2023-02-27 10:14:05 +0000

    Construtores

---
+++ aula29/Carro2.java
@@ -0,0 +1,23 @@
+package com.keven.cursojava.aula29;
+
+public class Carro2 {
+	
+	//atributos
+			String marca;
+			String modelo;
+			int numPassageiros;
+			double capCombustivel;
+			double consumoCombustivel;
+			public Carro2(String marca, String modelo, int numPassageiros, double capCombustivel,
+					double consumoCombustivel) {
+				super();
+				this.marca = marca;
+				this.modelo = modelo;
+				this.numPassageiros = numPassageiros;
+				this.capCombustivel = capCombustivel;
+				this.consumoCombustivel = consumoCombustivel;
+			}
+			
+			
+
+}
---
+++ aula29/TesteCarro.java
@@ -0,0 +1,32 @@
+package com.keven.cursojava.aula29;
+
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Carro van = new Carro();
+		
+		van.marca = "Fiat";
+		van.modelo = "Ducato";
+		//van.numPassageiros = 10;
+		van.capCombustivel = 100;
+		van.consumoCombustivel = 0.2;
+		
+		System.out.println(van.numPassageiros);
+		
+		Carro van2 = new Carro("Fiat", "ducato", 10, 100, 0.2);
+		van2.modelo = "ducato";
+		
+		System.out.println(van2.marca);
+		System.out.println(van2.modelo);
+		System.out.println(van2.numPassageiros);
+		System.out.println(van2.capCombustivel);
+		System.out.println(van2.consumoCombustivel);
+		
+		
+
+	}
+
+}

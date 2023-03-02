a1672986897caed0cb20761fc4db363b0d838e85
KevenMartins <124926761+KevenMartins@users.noreply.github.com>
2023-03-02 11:57:39 +0000

    Palavra chave final + constantes

---
+++ aula42/Constantes.java
@@ -0,0 +1,8 @@
+package com.keven.cursojava.aula42;
+
+public final class Constantes {
+	
+	public static final String URL_BLOG = "https://github.com/KevenMartins";
+	public static final String CURSO_COMPLETO = "https://github.com/KevenMartins/curso-java-basico";
+
+}
---
+++ aula42/Teste.java
@@ -0,0 +1,15 @@
+package com.keven.cursojava.aula42;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Pessoa pessoa = new Pessoa();
+		Aluno aluno = new Aluno();
+		Professor professor = new Professor();
+		
+			System.out.println(Constantes.CURSO_COMPLETO);
+
+	}
+
+}

fb0057f85f13b2bc4f348f69b0645b4d0f3effa8
KevenMartins <124926761+KevenMartins@users.noreply.github.com>
2023-03-02 11:34:33 +0000

    Herança: classes abstratas

---
+++ aula41/Aluno.java
@@ -0,0 +1,62 @@
+package com.keven.cursojava.aula41;
+
+public class Aluno extends Pessoa{
+	
+	private String curso;
+	private Double[] notas;
+	
+	
+	public Aluno() {
+		super();
+	}
+	
+	
+	public Aluno(String nome, String endereco, String telefone, String cpf, String telemovel) {
+		super(nome, endereco, telefone, cpf, telemovel);
+		
+		this.curso = curso;
+		
+	}
+
+
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public Double[] getNotas() {
+		return notas;
+	}
+	public void setNotas(Double[] notas) {
+		this.notas = notas;
+	}
+	
+	public double calcularMedia() {
+		return 0;
+	}
+	public boolean vereficarAprovado() {
+		return true;
+	}
+	
+	public void metodoQualquer() {
+		super.setCpf("23423");
+		this.setCpf("46345");
+	}
+	
+	public String obterEtiquetaEndereco() {
+		
+		String s = "Enderecço do Aluno : ";
+		s += this.getEndereco();
+		return s;
+	}
+
+
+	@Override
+	public void imprimirEtiquetaEndereco() {
+		
+		System.out.println(this.obterEtiquetaEndereco());
+		
+	}
+
+}

e59258a24a9502e6a7b0a71fbf9cbe7723d230fb
KevenMartins <124926761+KevenMartins@users.noreply.github.com>
2023-03-08 14:46:26 +0000

    Exceptions: tipos de erros no java

---
+++ aula51/AulaTeorica.java
@@ -0,0 +1,10 @@
+package com.keven.cursojava.aula51;
+
+public class AulaTeorica {
+	
+	//Nesta aula não temos código fonte, apenas teoria
+	
+	//link artigo para aprender inglês
+	//https://java.mn/2014/02/23/ler-ingles/
+
+}
